446e1c7deca5e9bc44ef96f65beca455e006c56c
Emmanuel-Odero <emmanuelodero808@gmail.com>
2025-08-02 14:54:52 +0300

    init: Add transaction.cpp for transaction creation, signing, and verification

---
+++ src/core/transaction.cpp
@@ -0,0 +1,7 @@
+#include "transaction.h"
+#include "utils.h"
+#include "logger.h"
+#include "json.hpp"
+
+// Transaction implementation file - empty as all the functions are already defined in the header file
+// This file is kept for potential future implementation details that should be moved out of the header
