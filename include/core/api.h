3d91f9a71b955182cd973643a56fd749a7615a40
Emmanuel-Odero <emmanuelodero808@gmail.com>
2025-08-02 14:55:17 +0300

    init: Add api.h for REST API server interface

---
+++ include/core/api.h
@@ -0,0 +1,30 @@
+#ifndef API_H
+#define API_H
+
+#include "blockchain.h"
+#include <thread>
+#include <atomic>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+class API {
+private:
+    Blockchain& blockchain;
+    std::atomic<bool> running;
+    std::thread server_thread;
+    int server_fd;
+    
+    void serverLoop();
+    void handleClient(int client_fd, struct sockaddr_in client_addr);
+    std::string generateResponse(const std::string& method, const std::string& path, const std::string& body);
+
+public:
+    API(Blockchain& blockchain);
+    ~API();
+    
+    void start(int port);
+    void stop();
+    bool isRunning() const { return running; }
+};
+
+#endif // API_H
